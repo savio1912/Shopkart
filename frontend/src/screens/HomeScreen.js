198299375511c40d8b246d1eb9b0a28f41792a58
Savio <iamsavio19@gmail.com>
2023-04-18 00:36:37 +0530

    useState replaced by useReducer and added loading and error divisions

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,23 +1,40 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Product from "../components/Product";
 import { Helmet } from "react-helmet-async";
+import logger from "use-reducer-logger";
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { ...state, loading: false, products: action.payload };
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 const HomeScreen = () => {
-  const [data, setData] = useState([]);
+  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+    loading: true,
+    error: "",
+    products: [],
+  });
+
   useEffect(() => {
     async function fetchData() {
-      const result = await axios.get("api/products");
-      setData(result.data);
+      dispatch({ type: "FETCH_REQUEST" });
+      try {
+        const result = await axios.get("api/products");
+        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+      } catch (err) {
+        dispatch({ type: "FETCH_FAIL", payload: err.message });
+      }
     }
-    fetchData()
-      .then(() => {
-        console.log("api fetch success");
-      })
-      .catch((err) => {
-        console.log(`api fetch failed ${err}`);
-      });
+    fetchData();
   }, []);
 
   return (
@@ -25,14 +42,21 @@ const HomeScreen = () => {
       <Helmet>
         <title>ShopKart</title>
       </Helmet>
-      <h1>Featured Products</h1>
+      <h1 className="homescreentitle">Featured Products</h1>
+
       <div className="products">
         <Row>
-          {data.map((product) => (
-            <Col sm={6} md={4} lg={3} className="mb-3">
-              <Product product={product}></Product>
-            </Col>
-          ))}
+          {loading ? (
+            <div>Loading....</div>
+          ) : error ? (
+            <div>Error...</div>
+          ) : (
+            products.map((product) => (
+              <Col sm={6} md={4} lg={3} className="mb-3">
+                <Product product={product}></Product>
+              </Col>
+            ))
+          )}
         </Row>
       </div>
     </>
