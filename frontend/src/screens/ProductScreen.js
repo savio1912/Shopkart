4b1b56490311c9c9e7de77691a3f071da8eae228
Savio <iamsavio19@gmail.com>
2023-04-17 13:52:15 +0530

    Fetching product data from the backend api, display the product details(image, stock, rating, price, Added dynamic tilte using helmet package)

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -3,6 +3,10 @@ import { useEffect, useState } from "react";
 import Col from "react-bootstrap/esm/Col";
 import Row from "react-bootstrap/esm/Row";
 import { useParams } from "react-router-dom";
+import Rating from "../components/Rating";
+import Card from "react-bootstrap/Card";
+import Badge from "react-bootstrap/Badge";
+import { Helmet } from "react-helmet-async";
 
 const ProductScreen = () => {
   const [data, setData] = useState({});
@@ -18,15 +22,44 @@ const ProductScreen = () => {
 
   return (
     <div>
+      <Helmet>
+        <title>{data.name}</title>
+      </Helmet>
       <Row>
         <Col sm={12} md={6} lg={6}>
           <img className="productimage" src={data.image} alt={data.name} />
         </Col>
         <Col sm={12} md={3} lg={3}>
-          <p>{data.name}</p>
+          <ul className="unordered-list">
+            <li>
+              <h1>{data.name}</h1>
+            </li>
+            <hr></hr>
+            <li>
+              <Rating rating={data.rating} numReviews={data.numReviews} />
+            </li>
+            <hr></hr>
+            <li> Price : {data.price}</li>
+            <hr></hr>
+            <li> Description : {data.description}</li>
+          </ul>
         </Col>
         <Col sm={12} md={3} lg={3}>
-          {data.category}
+          <Card>
+            <Card.Body>
+              <ul className="unordered-list">
+                <li>Price: {data.price}</li>
+                <hr></hr>
+                <li>
+                  {data.countInStock >= 1 ? (
+                    <Badge bg="success">In stock</Badge>
+                  ) : (
+                    <Badge bg="danger">Unavailable</Badge>
+                  )}
+                </li>
+              </ul>
+            </Card.Body>
+          </Card>
         </Col>
       </Row>
     </div>
