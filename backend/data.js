f6609cb42c2f9e57feb1ca4b62e9fbf520e69241
Savio <iamsavio19@gmail.com>
2023-04-19 17:53:36 +0530

    id added

--- backend/data.js
+++ backend/data.js
@@ -1,6 +1,7 @@
 const data = {
   products: [
     {
+      _id: "1",
       name: "Nike Slim Shirt",
       slug: "nike-slim-shirt",
       category: "Shirts",
@@ -13,6 +14,7 @@ const data = {
       description: "high quality shirt",
     },
     {
+      _id: "2",
       name: "Nike Medium Shirt",
       slug: "nike-medium-shirt",
       category: "Shirts",
@@ -25,6 +27,7 @@ const data = {
       description: "high quality shirt",
     },
     {
+      _id: "3",
       name: "Nike Slim Pant",
       slug: "nike-slim-pant",
       category: "Pants",
@@ -37,6 +40,7 @@ const data = {
       description: "high quality pant",
     },
     {
+      _id: "4",
       name: "Nike casual Pant",
       slug: "nike-casual-pant",
       category: "Pants",
