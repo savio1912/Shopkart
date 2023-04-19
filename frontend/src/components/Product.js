3c135eb64c51b7db449cc37de273e103116accdb
Savio <iamsavio19@gmail.com>
2023-04-20 01:28:47 +0530

    add to cart functionality is added

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,9 +1,30 @@
 import Button from "react-bootstrap/Button";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Card from "react-bootstrap/Card";
 import Rating from "./Rating";
+import { useContext } from "react";
+import { Store } from "../Store";
+import axios from "axios";
 const Product = (props) => {
+  const navigate = useNavigate();
+  const { state, dispatch } = useContext(Store);
   const { product } = props;
+
+  const addToCartHandler = async () => {
+    const existingItem = state.cart.cartItems.find(
+      (item) => item._id === product._id
+    );
+    const quantity = existingItem ? existingItem.quantity + 1 : 1;
+    const result = await axios.get(`/api/products/${product._id}`);
+    const item = result.data;
+    if (item.countInStock < quantity) {
+      window.alert("Sorry product is out of stock");
+      return;
+    }
+    dispatch({ type: "ADD_TO_CART", payload: { ...item, quantity } });
+    navigate("/cart");
+  };
+
   return (
     <Card className="product" key={product.slug}>
       <Link to={`/product/${product.slug}`}>
@@ -16,7 +37,14 @@ const Product = (props) => {
         <Card.Text>{product.price}</Card.Text>
         <Rating rating={product.rating} numReviews={product.numReviews} />
         <Card.Text>{product.category}</Card.Text>
-        <Button> Add to cart</Button>
+        {product.countInStock > 0 ? (
+          <Button onClick={addToCartHandler}> Add to cart</Button>
+        ) : (
+          <Button disabled variant="light">
+            {" "}
+            Out of Stock
+          </Button>
+        )}
       </Card.Body>
     </Card>
   );
