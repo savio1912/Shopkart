0908af2b90301c288a67478e8b7088eb792f5597
Savio <iamsavio19@gmail.com>
2023-04-19 17:54:25 +0530

    created a backend api to access individual products

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,17 @@ app.get("/api/products/slug/:slug", (req, res) => {
     res.status(404).send({ message: "Product Not found" });
   }
 });
+app.get("/api/products/:id", (req, res) => {
+  const product = data.products.find((x) => {
+    return x._id === req.params.id;
+  });
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product Not found" });
+  }
+});
+
 const port = process.env.port || 5000;
 app.listen(port, () => {
   console.log("App running");
