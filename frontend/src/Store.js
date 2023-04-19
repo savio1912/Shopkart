e53ebdf27acebfd05f5c6c55702823b1bbba7154
Savio <iamsavio19@gmail.com>
2023-04-19 17:50:11 +0530

    reducer function only adds the new item into the cart and just increasing the quatity for the existinf products

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -9,13 +9,16 @@ const initialState = {
 const reducer = (state, action) => {
   switch (action.type) {
     case "ADD_TO_CART":
-      return {
-        ...state,
-        cart: {
-          ...state.cart,
-          cartItems: [...state.cart.cartItems, action.payload],
-        },
-      };
+      const newItem = action.payload;
+      const existingItem = state.cart.cartItems.find(
+        (item) => item._id === action.payload._id
+      );
+      const cartItems = existingItem
+        ? state.cart.cartItems.map((item) =>
+            item._id === existingItem._id ? newItem : item
+          )
+        : [...state.cart.cartItems, newItem];
+      return { ...state, cart: { ...state.cart, cartItems } };
     default:
       return state;
   }
